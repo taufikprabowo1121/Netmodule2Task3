dc111c39a8edcf3fd87751c3366d291b46fd4753
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 16:57:02 +0700

    NetModul

---
+++ NetModule2Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetModule2Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int tmp = 0;
+            int tampungan =0 ;
+            double hitung;
+            Console.Write("Masukan Angaka :");
+            n = int.Parse(Console.ReadLine());
+            int[] larik = new int[n];
+            for(int i=0; i<n; i++)
+            {
+                Console.Write("Masukan Array Ke ");
+                larik[i] = int.Parse(Console.ReadLine());
+           
+            }
+           
+            for(int k=0; k<n; k++)
+            {
+                tmp += larik[k] ;
+                tampungan = n;  
+            }   
+
+            hitung = tmp / tampungan;
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Nilai Rata Rata :" + hitung);
+            Console.ReadKey();
+        }
+    }
+}
